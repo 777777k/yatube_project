92837a9182b9af7710fe04e5e1bbf3276c957b59
Дмитрий Крупин <thediman23@gmail.com>
2023-02-16 09:18:03 +0300

    Добавление словаря контекста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = "Это главная страница проекта Yatube"
+    title = 'Главная страница'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template, {'slug': slug})
+    text = "Здесь будет информация о группах проекта Yatube"
+    title = 'Информация о группах'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
