21ca3d88e742f0ec787d269b519a40e44d3cb9e7
Дмитрий Крупин <thediman23@gmail.com>
2023-03-08 17:00:42 +0300

    Info output from DB to pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from .models import Post, Group
 
-# Create your views here.
+
 def index(request):
-    template = 'posts/index.html'
-    text = "Это главная страница проекта Yatube"
-    title = 'Главная страница'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    text = "Здесь будет информация о группах проекта Yatube"
-    title = 'Информация о группах'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
